8ab3b3beec4e51323aa2551a5c4d3376ba463e1d
WenHao2130 <WenHao2130@outlook.com>
2025-04-13 07:05:49 +0800

    PAFM00: remove useless `BOARD_KERNEL_CMDLINE`
    Signed-off-by: WenHao2130 <WenHao2130@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,6 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 \
                         androidboot.configfs=true \
                         loop.max_part=7 \
                         androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
